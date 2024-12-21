d297f0932250ca86b2c46bf3c3259789b12f56e6
Rahul Sagar <sagarrahul045@gmail.com>
2024-12-21 09:18:21 +0530

    first commit

---
+++ CRUD-Application/src/App.jsx
@@ -0,0 +1,159 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+import EmployeeData from './EmployeeData'
+
+function App() {
+  const [data, setData] = useState([]);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [age, setAge] = useState('');
+  const [id, setId] = useState(0);
+  const [isUpdate, setIsUpdate] = useState(false);
+
+  useEffect(()=>{
+    setData(EmployeeData)
+  },[]);
+
+  const handleEdit=(id)=>{
+    const dt = data.filter((item) => {
+      return item.id === id
+    })
+    if(dt !== undefined){
+      setIsUpdate(true)
+      setId(id)
+      setFirstName(dt[0].firstName);
+      setLastName(dt[0].lastName);
+      setAge(dt[0].age);
+    }
+  }
+
+  const handleDelete=(id)=>{
+    if(id > 0){
+      if(window.confirm("Are you sure to delete this item?")){
+        const dt=data.filter((item) => {
+          return item.id !== id
+        });
+        setData(dt);
+      }
+    }
+  }
+
+  const handleSave = (e) => {
+    let error = ''
+
+    if(firstName === '')
+      error += 'First Name is required';
+
+    if(lastName === '')
+      error += 'Last Name is required';
+
+    if(age === '')
+      error += 'Age is required';
+
+    if(error <= 0){
+    e.preventDefault();
+    const dt = [...data];
+    const newObject = {
+      id: data.length + 1,
+      firstName: firstName,
+      lastName: lastName,
+      age: age
+    }
+    dt.push(newObject);
+    setData(dt);
+    }
+    else{
+      alert(error);
+    }
+  }
+
+  const handleClear = () => {
+      setId(0)
+      setFirstName('');
+      setLastName('');
+      setAge('');
+      setIsUpdate(false);
+  }
+
+  const handleUpdate = () => {
+    const index = data.map((item) => {
+      return item.id
+    }).indexOf(id);
+
+    const dt = [...data];
+    dt[index].firstName = firstName;
+    dt[index].lastName = lastName;
+    dt[index].age = age;
+
+    setData(dt);
+    handleClear();
+  }
+
+  return (
+    <>
+
+      <div style={{display: 'flex' , justifyContent: 'center' , marginTop: '10px' , marginBottom: '10px'}}>
+        <div>
+          <label>First Name :
+            <input type="text" placeholder='Enter first name' value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+          </label>
+        </div>
+        <div>
+          <label>Last Name :
+            <input type="text" placeholder='Enter Last name' value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+          </label>
+        </div>
+        <div>
+          <label>Age :
+            <input type="text" placeholder='Enter Age' value={age} onChange={(e) => setAge(e.target.value)}/>
+          </label>
+        </div>
+        <div style={{marginLeft:'10px'}}>
+          {
+            !isUpdate ?
+            <button style={{marginLeft:'10px'}} className='btn btn-primary' onClick={(e)=>handleSave(e)}>Save</button>
+            :
+            <button className='btn btn-primary' onClick={()=>handleUpdate()}>Update</button>
+          }
+          
+          <button className='btn btn-danger' onClick={()=>handleClear()}>Clear</button>
+        </div>
+      </div>
+
+      <table className='table tbale-hover'>
+        <thead>
+          <tr>
+            <th>Sr.No.</th>
+            <th>Id</th>
+            <th>FirstName</th>
+            <th>LastName</th>
+            <th>Age</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((item, index)=>{
+              return(
+                <tr key={index}>
+                  <td>{index+1}</td>
+                  <td>{item.id}</td>
+                  <td>{item.firstName}</td>
+                  <td>{item.lastName}</td>
+                  <td>{item.age}</td>
+                  <td>
+                    <button className='btn btn-primary' onClick={()=>handleEdit(item.id)}>Edit</button>&nbsp;
+                    <button className='btn btn-danger' onClick={()=>handleDelete(item.id)}>Delete</button>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default App
---
+++ CRUD-Application/src/EmployeeData.jsx
@@ -0,0 +1,28 @@
+const EmployeeData = [
+    {
+        id: 1,
+        firstName: "Rahul",
+        lastName: "Sagar",
+        age: 26
+    },
+    {
+        id: 2,
+        firstName: "Gaurav",
+        lastName: "Sagar",
+        age: 28
+    },
+    {
+        id: 3,
+        firstName: "Anshul",
+        lastName: "Sharma",
+        age: 26
+    },
+    {
+        id: 4,
+        firstName: "Dheeraj",
+        lastName: "Rathaur",
+        age: 26
+    },
+]
+
+export default EmployeeData
